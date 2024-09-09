eb3046e6f46e8ae1dc3e005ba1fb6f766cab0b65
Andreas Bergström <skrap@andreasb.se>
2024-09-09 19:57:43 +0200

    Added booking overview function, showing bookings from today onwards

--- functions/bookingOverview/index.js
+++ functions/bookingOverview/index.js
@@ -1,10 +1,19 @@
 import { db } from "../../services/db.js";
 import { sendResponse, sendError } from "../../services/responses.js";
+
 export const handler = async (event) => {
+  const today = new Date().toISOString().split("T")[0];
   try {
-    const { Items } = await db.scan({ TableName: "bonzai_bookings" });
+    const params = {
+      TableName: "bonzai_bookings",
+      FilterExpression: "date_in >= :today",
+      ExpressionAttributeValues: {
+        ":today": today,
+      },
+    };
+    const { Items } = await db.scan(params);
     return sendResponse(Items);
   } catch (error) {
-    return sendError(404, error);
+    return sendError(500, error);
   }
 };
