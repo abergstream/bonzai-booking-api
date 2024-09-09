ac5caf4b368892eb775ca611592b7bcdcd7dce4c
Andreas Bergström <skrap@andreasb.se>
2024-09-09 20:38:00 +0200

    Added delete functionality with 2 days limit

--- functions/bookingOverview/index.js
+++ functions/bookingOverview/index.js
@@ -2,7 +2,9 @@ import { db } from "../../services/db.js";
 import { sendResponse, sendError } from "../../services/responses.js";
 
 export const handler = async (event) => {
+  // Get todays date in YYYY-mm-dd format
   const today = new Date().toISOString().split("T")[0];
+
   try {
     const params = {
       TableName: "bonzai_bookings",
@@ -11,7 +13,9 @@ export const handler = async (event) => {
         ":today": today,
       },
     };
+
     const { Items } = await db.scan(params);
+
     return sendResponse(Items);
   } catch (error) {
     return sendError(500, error);
--- functions/deleteBooking/index.js
+++ functions/deleteBooking/index.js
@@ -1,8 +1,56 @@
+import { db } from "../../services/db.js";
+import { sendResponse, sendError } from "../../services/responses.js";
+
 export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
-  };
+  // Get todays date in YYYY-mm-dd format
+
+  const { bookingNumber } = JSON.parse(event.body);
+
+  try {
+    const params = {
+      TableName: "bonzai_bookings",
+      Key: {
+        id: bookingNumber,
+      },
+    };
+
+    const { Item } = await db.get(params);
+
+    // If no item is returned, send a 404 error
+    if (!Item) {
+      return sendError(
+        404,
+        `No booking found with reference number: ${bookingNumber}`
+      );
+    }
+    const date_today = new Date().toISOString().split("T")[0];
+    const date_in = Item.date_in;
+
+    // Convert dates to Date objects
+    const todayDate = new Date(date_today);
+    const inDate = new Date(date_in);
+
+    // Calculate the difference in time (milliseconds)
+    const differenceInTime = inDate - todayDate;
+
+    // Convert the time difference to days
+    const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+
+    // If difference in days is less than 2, return a bad request
+    if (differenceInDays < 2) {
+      return sendError(
+        400,
+        "You cannot cancel your stay later than 2 days before check-in."
+      );
+    }
+
+    try {
+      await db.delete(params);
+      return sendResponse({ message: "Booking removed" });
+    } catch (error) {
+      return sendError(500, error);
+    }
+  } catch (error) {
+    return sendError(500, error);
+  }
 };
